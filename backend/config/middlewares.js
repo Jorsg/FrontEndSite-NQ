25dfb5028fcf4caf28736ee549cda183af445f03
dev <Jorsg>
2026-01-19 17:56:17 -0500

    Configuration Strapi CMS, it can create a new articles, add new feature WhatsApp comunication

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -5,7 +5,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:8080', 'http://127.0.0.1:8080', 'http://localhost:5500'],
+      origin: ['http://localhost:8080', 'http://127.0.0.1:8080', 'http://localhost:5500', 'http://localhost:56786', 'http://127.0.0.1:5500', 'http://127.0.0.1:56786', '*'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
